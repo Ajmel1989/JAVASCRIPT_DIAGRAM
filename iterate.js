b36fcedb80cae2fa736afea7aab02c81fc85a05f
Hamd <ajmelpareeth@gmail.com>
2024-08-28 22:43:37 +0530

    BASICS OF JAVASCRIPT

---
+++ iterate.js
@@ -0,0 +1,36 @@
+
+// var b=5
+// var a=""
+for(i=0;i<=5;i++){
+    var a=""
+    for(j=1;j<=i;j++){
+       a=a+" * " 
+    }
+    console.log(a)
+
+}
+console.log("================")
+
+
+for(i=1;i<=5;i++){
+    var a=""
+    for(j=5;j>=i;j--){
+       a=a+" * " 
+    }
+    console.log(a)
+
+}
+console.log("================")
+
+
+for(i=1;i<=5;i++){
+    
+    for(j=5;j>=i;j--){
+       
+    }var a=" "
+    for(k=1;k<=5;k++){
+        a=a+" * " 
+    }
+    console.log(a)
+
+}
